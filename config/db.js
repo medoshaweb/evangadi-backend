7b4a1851168d5049121dc6973a59e17712da0718
medoshaweb <hailugs@yahoo.com>
2025-10-28 16:49:12 -0700

    first

--- config/db.js
+++ config/db.js
@@ -7,9 +7,7 @@ const db = await mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  
 });
 
 console.log("✅ MySQL connected");
