0b756b42eca580ca80abcf42996c2489d7ca4625
medoshaweb <hailugs@yahoo.com>
2025-10-29 12:39:35 -0700

    eight

--- server.js
+++ server.js
@@ -17,18 +17,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use("/", installRoutes);
-app.use(
-  cors({
-    origin: [
-      process.env.FRONTEND_URL || "https://evangadi-frontend-sand.vercel.app"
-
-      // "http://localhost:5173", // for local development
-    ],
-    credentials: true,
-  })
-);
-// app.options("*", cors());
-
+app.use(cors());
+   
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
